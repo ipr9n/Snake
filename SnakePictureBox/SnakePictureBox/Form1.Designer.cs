b0b4af35381647180fd4402cee6a58400d9ac098
ipr9n <57600027+ipr9n@users.noreply.github.com>
2019-11-11 12:08:06 +0300

    Add files via upload
    Расчертил поле.  Двигается, умирает возле стен, видит еду , но не ест

---
+++ SnakePictureBox/SnakePictureBox/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace SnakePictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SnakeHead = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SnakeHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(601, 601);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SnakeHead
+            // 
+            this.SnakeHead.Location = new System.Drawing.Point(771, 189);
+            this.SnakeHead.Name = "SnakeHead";
+            this.SnakeHead.Size = new System.Drawing.Size(100, 50);
+            this.SnakeHead.TabIndex = 1;
+            this.SnakeHead.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 605);
+            this.Controls.Add(this.SnakeHead);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SnakeHead)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox SnakeHead;
+    }
+}
+
---
+++ SnakePictureBox/SnakePictureBox/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakePictureBox
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox food;
+        const int xh = 120, yh = 120, size = 20;
+        public int SnakeLengths = 0;
+        public string Move = "";
+       // private List<PictureBox> trails = new List<PictureBox>();
+
+
+            /* public void addTrail(Point last, int SnakeLength)
+        {
+            trails.Add(new PictureBox());
+            this.Controls.Add(trails[SnakeLength]);
+            trails[SnakeLength].Location = last;
+            trails[SnakeLength].Size = new Size(size,size);
+            trails[SnakeLength].BackColor = Color.Yellow;
+
+
+        }*/
+        public Form1()
+        {
+            food = new PictureBox();
+            this.Controls.Add(food);
+            food.BackColor = Color.Green;
+            food.Size = new Size(size, size);
+            food.Location = new Point(xh, yh);
+            InitializeComponent();
+
+
+        }
+
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SnakeHead.Width = size;
+            SnakeHead.Height = size;
+            SnakeHead.Location = new Point(0, 0);
+            SnakeHead.BackColor = Color.Blue;
+            pictureBox1.Location = new Point(0,0);
+            timer1.Interval = 250;
+            timer1.Start();
+            
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "Right")
+                Move = "Right";
+            if (e.KeyCode.ToString() == "Left")
+                Move = "Left";
+            if (e.KeyCode.ToString() == "Down")
+                Move = "Down";
+            if (e.KeyCode.ToString() == "Up")
+                Move = "Up";
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("GameOver");
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (Move)
+            {
+                case "Right":
+                    SnakeHead.Location = new Point(SnakeHead.Location.X + size, SnakeHead.Location.Y);
+                    if (SnakeHead.Location.X == food.Location.X && SnakeHead.Location.Y == food.Location.Y)  // Проверка видит ли змея еду
+                    {
+                        GameOver();
+                        //Generate food();
+                        // Add trail();
+                    }
+                    if (SnakeHead.Location.X >= pictureBox1.Height - (size * 2)) // Смерть при врезании в стену
+                        GameOver();
+                    break;
+                case "Left":
+                    SnakeHead.Location = new Point(SnakeHead.Location.X - size, SnakeHead.Location.Y);
+                    if (SnakeHead.Location.X == food.Location.X && SnakeHead.Location.Y == food.Location.Y)// Проверка видит ли змея еду
+                    {
+                        GameOver();
+                        //Generate food();
+                        
+                    //     addTrail(new Point(SnakeHead.Location.X-size,SnakeHead.Location.Y),SnakeLengths);
+                     //    SnakeLengths++;
+                    }
+                    if (SnakeHead.Location.X < 0) // Смерть при врезании в стену
+                        GameOver();
+                    break;
+                case "Up":
+                    SnakeHead.Location = new Point(SnakeHead.Location.X, SnakeHead.Location.Y-size);
+                    if (SnakeHead.Location.X == food.Location.X && SnakeHead.Location.Y == food.Location.Y)// Проверка видит ли змея еду
+                    {
+                        GameOver();
+                        //Generate food();
+                        // Add trail();
+                    }
+                    if (SnakeHead.Location.Y < 0) // Смерть при врезании в стену
+                        GameOver();
+                    break;
+                case "Down":
+                    SnakeHead.Location = new Point(SnakeHead.Location.X, SnakeHead.Location.Y+size);
+                    if (SnakeHead.Location.X == food.Location.X && SnakeHead.Location.Y == food.Location.Y)// Проверка видит ли змея еду
+                    {
+                        GameOver();
+                        //Generate food();
+                        // Add trail();
+                    }
+                    if (SnakeHead.Location.Y >= pictureBox1.Width-size) // Смерть при врезании в стену
+                        GameOver();
+                    break;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+         {
+             //                      Заливка поля                            ///
+
+             for (int x = 0; x < this.Width; x += size)
+                 e.Graphics.DrawLine(Pens.Black, x, 0, x, this.Height);
+             for (int y = 0; y < this.Height; y += size)
+                 e.Graphics.DrawLine(Pens.Black, 0, y, this.Width, y);
+             ///                                                     /////////////
+
+
+         }
+    }
+}
