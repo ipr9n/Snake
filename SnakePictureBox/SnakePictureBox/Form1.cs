d35338a9de9251c78632c3df5387ce6d93df442f
ipr9n <57600027+ipr9n@users.noreply.github.com>
2019-11-11 22:03:29 +0300

    Убрал лишние переменные. Поменял координаты на индексы поля.

--- SnakePictureBox/SnakePictureBox/Form1.cs
+++ SnakePictureBox/SnakePictureBox/Form1.cs
@@ -7,123 +7,127 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
-        const int SizeOfSquare = 20;
-        private static int ofx = 0, ofy = 0;
-        List<Point> SnakeBody = new List<Point>();
-        private new Point food = new Point(120, 120);
-        Keys SnakeDirection;
+        const int SquareSize = 20;
+        Point headLocation = new Point(0, 0);
+        List<Point> snakeBody = new List<Point>();
+        private new Point food = new Point(7, 10);
+        Keys snakeDirection;
         Random random = new Random();
 
         public Form1()
         {
-            SnakeBody.Add(new Point(0, 0));
+            snakeBody.Add(new Point(0, 0));
             InitializeComponent();
         }
+
         private void GenerateFood()
         {
             timer1.Interval -= 5;
-            food = new Point(random.Next(SizeOfSquare) * SizeOfSquare, random.Next(SizeOfSquare) * SizeOfSquare);
-            foreach (var Snake in SnakeBody)
+            food = new Point(random.Next(SquareSize), random.Next(SquareSize));
+            foreach (var snakePart in snakeBody)
             {
-                if (food == Snake)
+                if (food == snakePart)
                 {
-                    food = new Point(random.Next(SizeOfSquare) * SizeOfSquare, random.Next(SizeOfSquare) * SizeOfSquare);
+                    food = new Point(random.Next(SquareSize), random.Next(SquareSize));
                     break;
                 }
             }
         }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             timer1.Interval = 250;
         }
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             timer1.Start();
             switch (e.KeyCode)
             {
-                case Keys.Right when SnakeDirection == Keys.Right && SnakeBody.Count > 1:
-                    SnakeDirection = Keys.Left;
+                case Keys.Right when snakeDirection == Keys.Left && snakeBody.Count > 1:
+                    snakeDirection = Keys.Left;
                     break;
                 case Keys.Right:
-                case Keys.Left when SnakeDirection == Keys.Right && SnakeBody.Count > 1:
-                    SnakeDirection = Keys.Right;
+                case Keys.Left when snakeDirection == Keys.Right && snakeBody.Count > 1:
+                    snakeDirection = Keys.Right;
                     break;
                 case Keys.Left:
-                    SnakeDirection = Keys.Left;
+                    snakeDirection = Keys.Left;
                     break;
-                case Keys.Down when SnakeDirection == Keys.Up && SnakeBody.Count > 1:
-                    SnakeDirection = Keys.Up;
+                case Keys.Down when snakeDirection == Keys.Up && snakeBody.Count > 1:
+                    snakeDirection = Keys.Up;
                     break;
                 case Keys.Down:
-                case Keys.Up when SnakeDirection == Keys.Down && SnakeBody.Count > 1:
-                    SnakeDirection = Keys.Down;
+                case Keys.Up when snakeDirection == Keys.Down && snakeBody.Count > 1:
+                    snakeDirection = Keys.Down;
                     break;
                 case Keys.Up:
-                    SnakeDirection = Keys.Up;
+                    snakeDirection = Keys.Up;
                     break;
             }
         }
+
         private void GameOver()
         {
             timer1.Stop();
             MessageBox.Show("GameOver");
-            SnakeBody.Clear();
+            snakeBody.Clear();
+            headLocation = new Point(0, 0);
             timer1.Interval = 250;
-            SnakeBody.Add(new Point(0, 0));
-            SnakeDirection = Keys.A;
+            snakeBody.Add(new Point(0, 0));
+            snakeDirection = Keys.A;
         }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            switch (SnakeDirection)
+            switch (snakeDirection)
             {
                 case Keys.Right:
-                    ofx = 1;
-                    ofy = 0;
+                    headLocation.X += 1;
 
                     break;
                 case Keys.Left:
-                    ofx = -1;
-                    ofy = 0;
+                    headLocation.X -= 1;
 
                     break;
                 case Keys.Up:
-                    ofx = 0;
-                    ofy = -1;
+                    headLocation.Y -= 1;
 
                     break;
                 case Keys.Down:
-                    ofx = 0;
-                    ofy = 1;
+                    headLocation.Y += 1;
 
                     break;
             }
-            SnakeBody.Insert(0, new Point(SnakeBody[0].X + (ofx * SizeOfSquare), SnakeBody[0].Y + (ofy * SizeOfSquare)));
-            if (SnakeBody[0] != food)
-                SnakeBody.RemoveAt(SnakeBody.Count - 1);
-            else
-                GenerateFood();
-            pictureBox1.Refresh();
-        }
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            for (int x = 0; x < this.Width; x += SizeOfSquare)
-                e.Graphics.DrawLine(Pens.Black, x, 0, x, this.Height);
-            for (int y = 0; y < this.Height; y += SizeOfSquare)
-                e.Graphics.DrawLine(Pens.Black, 0, y, this.Width, y);
+            snakeBody.Insert(0, headLocation);
 
-            if (SnakeBody[0].X >= pictureBox1.Width || SnakeBody[0].X < 0 || SnakeBody[0].Y < 0 ||
-                SnakeBody[0].Y >= pictureBox1.Height)
-                GameOver();
-
-            for (int i = 1; i < SnakeBody.Count; i++)
-                if (SnakeBody[0] == SnakeBody[i])
+            for (int i = 1; i < snakeBody.Count; i++)
+                if (snakeBody[0] == snakeBody[i])
                     GameOver();
 
-            foreach (var Snake in SnakeBody)
-                e.Graphics.FillRectangle(Brushes.Green, Snake.X, Snake.Y, SizeOfSquare, SizeOfSquare);
+            if (snakeBody[0] != food)
+                snakeBody.RemoveAt(snakeBody.Count - 1);
+            else
+                GenerateFood();
 
-            e.Graphics.FillRectangle(Brushes.Blue, food.X, food.Y, SizeOfSquare, SizeOfSquare);
+            pictureBox1.Refresh();
+        }
 
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int x = 0; x < this.Width; x += SquareSize)
+                e.Graphics.DrawLine(Pens.Black, x, 0, x, this.Height);
+            for (int y = 0; y < this.Height; y += SquareSize)
+                e.Graphics.DrawLine(Pens.Black, 0, y, this.Width, y);
+
+            if (snakeBody[0].X * SquareSize >= pictureBox1.Width || snakeBody[0].X * SquareSize < 0 || snakeBody[0].Y * SquareSize < 0 ||
+                snakeBody[0].Y * SquareSize >= pictureBox1.Height)
+                GameOver();
+
+            foreach (var Snake in snakeBody)
+                e.Graphics.FillRectangle(Brushes.Green, Snake.X * SquareSize, Snake.Y * SquareSize, SquareSize, SquareSize);
+
+            e.Graphics.FillRectangle(Brushes.Blue, food.X * SquareSize, food.Y * SquareSize, SquareSize, SquareSize);
         }
     }
 }
